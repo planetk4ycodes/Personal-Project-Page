8d3af2bdf73d57e682c83cec34c53b889ee000fe
planetk4ycodes <99414925+planetk4ycodes@users.noreply.github.com>
2022-02-26 11:33:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function toggleDarkMode() {
     let bodyElement = document.getElementById("main-background");
     let darkModeButtonElement = document.getElementById("dark-mode-btn");
     if(bodyElement.style.background === "black"){
-        bodyElement.style.background = "#F9EAE1";
+        bodyElement.style.background = "rgb(213, 165, 240)";
         darkModeButtonElement.innerText = "Switch to Dark Mode";
     }
 
