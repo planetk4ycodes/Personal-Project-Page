3eb43b0b84fd5006fcb27abf4a40d9d0681df8fb
planetk4ycodes <99414925+planetk4ycodes@users.noreply.github.com>
2022-03-05 18:29:51 -0500

    updated main js

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-function toggleDarkMode() {
-    let bodyElement = document.getElementById("main-background");
-    let darkModeButtonElement = document.getElementById("dark-mode-btn");
-    if(bodyElement.style.background === "black"){
-        bodyElement.style.background = "rgb(213, 165, 240)";
-        darkModeButtonElement.innerText = "Switch to Dark Mode";
-    }
+let strawberry = document.getElementById('strawberry');
+let sparkles = document.getElementById('sparkles');
+let dessert = document.getElementById('dessert');
+let btn = document.getElementById('btn');
+let header = document.querySelector('header');
 
-    else{
-        bodyElement.style.background = "black";
-        darkModeButtonElement.innerHTML = "Switch to Light Mode";
-    }
-}
+window.addEventListener('scroll', function(){
+ let value = window.scrollY
+ sparkles.style.left = `${value * 0.25}px`;
+ strawberry.style.top = `${value * 1.05}px`;
+ dessert.style.top = `${value * 0.5}px`;
+ text.style.marginRight = `${value * 4}px`;
+ text.style.marginTop = `${value * 1.5}px`;
+ btn.style.marginTop = `${value * 1.5}px`;
+header.style.top = value `${value * 0.5}px`
+})
+
+var content = document.getElementsByTagName('body')[0];
+var darkMode = document.getElementById('dark-change');
+darkMode.addEventListener('click', function(){
+    darkMode.classList.toggle('active');
+    content.classList.toggle('night');
+})
+
